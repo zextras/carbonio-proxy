098b40b4f7a7c46599a31baf87c7d7b88015ef3b
Daniele Megna <daniele.megna@qmates.tech>
2024-02-07 18:24:43 +0100

    chore: improve install_packages - always use latest package

--- install_packages.sh
+++ install_packages.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 HOST=$1
 
-LAST_PACKAGE_PATH=$(ls artifacts/carbonio-proxy*.deb | tail -n 1)
+LAST_PACKAGE_PATH=$(ls -t artifacts/carbonio-proxy*.deb | head -n 1)
 PACKAGE_NAME=$(basename ${LAST_PACKAGE_PATH})
 
 rsync -acz artifacts/${PACKAGE_NAME} root@${HOST}:/tmp/
