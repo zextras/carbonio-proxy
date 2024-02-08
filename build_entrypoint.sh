a72dc50917f244b00d790973a6b10fe5bda033fa
Daniele Megna <daniele.megna@qmates.tech>
2024-02-08 11:55:22 +0100

    chore: add skip-sync option to yap build command :rocket:

--- build_entrypoint.sh
+++ build_entrypoint.sh
@@ -9,6 +9,6 @@ cp -r /project/package/* /tmp/build/
 cp -r /project/conf/nginx/errors/* /tmp/build/proxy/
 cp -r /project/conf/nginx/templates/* /tmp/build/proxy/
 
-yap build ubuntu-focal /tmp/build/
+yap build -s ubuntu-focal /tmp/build/
 
 cp /artifacts/* /project/artifacts/
