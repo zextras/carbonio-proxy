d68bf510defbf81ebf80959e2f11340a8e2b7c63
Davide Frison <36330277+frisonisland@users.noreply.github.com>
2025-07-02 09:35:13 +0200

    chore: define proxy dev container
    chore: define proxy dev container

---
+++ entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+sed -i -e "s#LDAP_URL#${LDAP_URL}#g" /localconfig/localconfig.xml
+sed -i -e "s/LDAP_ROOT_PASSWORD/${LDAP_ROOT_PASSWORD}/g" /localconfig/localconfig.xml
+sed -i -e "s/LDAP_ADMIN_PASSWORD/${LDAP_ADMIN_PASSWORD}/g" /localconfig/localconfig.xml
+sed -i -e "s/MARIADB_ROOT_PASSWORD/${MARIADB_ROOT_PASSWORD}/g" /localconfig/localconfig.xml
+sed -i -e "s/MARIADB_URL/${MARIADB_URL}/g" /localconfig/localconfig.xml
+sed -i -e "s/MARIADB_PORT/${MARIADB_PORT}/g" /localconfig/localconfig.xml
+sed -i -e "s/SERVER_HOSTNAME/${HOSTNAME}/g" /localconfig/localconfig.xml
+
+SERVER_EXISTS=$(/usr/bin/zmprov -l gs "${HOSTNAME}" 2>&1)
+if [[ $SERVER_EXISTS == *"account.NO_SUCH_SERVER"* ]]; then
+  echo "Creating server ${HOSTNAME}"
+  /usr/bin/zmprov -l cs "${HOSTNAME}" zimbraServiceInstalled proxy \
+                                      zimbraServiceEnabled proxy \
+                                      zimbraServiceInstalled memcached \
+                                      zimbraServiceEnabled memcached \
+                                      zimbraMemcachedBindAddress "${MEMCACHED_BIND_ADDRESS}" \
+                                      zimbraMemcachedBindPort "${MEMCACHED_BIND_PORT}" \
+                                      zimbraMailProxyPort 80 \
+                                      zimbraMailSSLProxyPort 443 \
+                                      zimbraReverseProxyStrictServerNameEnabled FALSE
+
+  echo "Server ${HOSTNAME} created"
+fi
+
+
+chmod +x /usr/bin/zmproxyconfgen
+
+/usr/bin/zmproxyconfgen
+
+/opt/zextras/common/sbin/nginx -c /opt/zextras/conf/nginx.conf -g "daemon off;"
